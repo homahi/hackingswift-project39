e156c04259f1b1220cf0526f39a66572c08984d7
homahi <homahi0128@gmail.com>
2018-02-23 17:28:03 +0900

    Unitテストのチュートリアルを実施

--- Project39Tests/Project39Tests.swift
+++ Project39Tests/Project39Tests.swift
@@ -21,16 +21,44 @@ class Project39Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count,18440, "AllWords was not 18440")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.wordCounts.count(for: "home"), 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "mortal"), 41, "Mortal does not appear 41 times")
+    }
+    
+    func testWordsLoadQuickly() {
+        measure {
+            _ = PlayData()
         }
     }
     
+    func testUserFilterWorks() {
+        let playData = PlayData()
+        
+        playData.applyUserFilter("100")
+        XCTAssertEqual(playData.filteredWords.count, 495)
+        
+        playData.applyUserFilter("1000")
+        XCTAssertEqual(playData.filteredWords.count, 55)
+        
+        playData.applyUserFilter("10000")
+        XCTAssertEqual(playData.filteredWords.count, 1)
+        
+        playData.applyUserFilter("test")
+        XCTAssertEqual(playData.filteredWords.count, 56)
+        
+        playData.applyUserFilter("swift")
+        XCTAssertEqual(playData.filteredWords.count, 7)
+        
+        playData.applyUserFilter("objective-c")
+        XCTAssertEqual(playData.filteredWords.count, 0)
+    }
+
 }
